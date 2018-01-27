f3c922f34442874123229366ac61a823144b293b
Flávio Carneiro da Silva <fmcarneirosilva@gmail.com>
2018-01-27 16:10:26 +0000

    dgfdfgdfgfdg

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const express = require('express');
 const http = require('http');
 const irc = require('node-irc');
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app).listen(3000);
 
 /*
   Iniciar e Configurar o Express
 */
   app.use(express.static(__dirname + '/public'));
-/*
-  Iniciar o Servidor
-*/
-app.listen(3000);
 
 /*
   Carregar o Socket.io e o Protocolo de IRC
