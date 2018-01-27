60b57415860634811eb84c5b3364b645756c3bd6
Flávio Carneiro da Silva <fmcarneirosilva@gmail.com>
2018-01-27 12:35:21 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+/*
+  Iniciar e Configurar o Express
+*/
+
+app.configure(function () {
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
+});
+
+/*
+  Iniciar o Servidor
+*/
+var server = http.createServer(app).listen(process.env.PORT || 3000);
+
+/*
+  Carregar o Socket.io e o Protocolo de IRC
+*/
+require('./lib/server.js')(server);
